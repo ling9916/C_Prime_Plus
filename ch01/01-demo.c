9609c2c183c8ae10a95b1d4421970150b2fa3546
ubuntu-wsl <wsl@gmail.com>
2023-05-02 13:13:32 +0800

    first commit

---
+++ ch01/01-demo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+  int dogs;
+
+  printf("How many dogs do you have?\n");
+  scanf("%d", &dogs);
+  printf("So you have %d dog(s)!\n", dogs);
+
+
+
+  return 0;
+}
